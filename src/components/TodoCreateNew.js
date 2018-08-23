5c6265bdd0883a499415c2b5a26566215e515d3a
Binh Nguyen <b.nguyen.sn@gmail.com>
2018-08-23 23:16:17 +0100

    Add strategy for <TodoCreateNew />

--- src/components/TodoCreateNew.js
+++ src/components/TodoCreateNew.js
@@ -2,10 +2,29 @@ import * as React from 'react';
 import TodoEdit from './TodoEdit';
 
 export default class TodoCreateNew extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoId: undefined,
+        }
+    }
+
+    // Occur when there are edits in the <TodoCreateNew /> box and user clicks
+    // outside of the box
+    addNewTodo = () => {
+        // Add data to database
+
+        // Update the DOM (add a new <TodoCard />)
+
+        // Reset <TodoCreateNew /> to a blank <TodoEdit />
+    };
+
     render() {
+        const {todoId} = this.state;
+
         return (
             <div className="todo-create-new">
-                <TodoEdit />
+                <TodoEdit todoId={todoId} />
             </div>
         )
     }
