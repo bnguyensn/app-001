d25308fcead1c85d213dbee97b7d7d0f1ffa7e8b
bnguyensn <b.nguyen.sn@gmail.com>
2018-12-15 09:31:53 +0700

    Move todo data files

--- src/utils/getTodos.js
+++ src/utils/getTodos.js
@@ -1,4 +1,4 @@
 export default async function getTodos() {
-    // return JSON.parse(await import('../data/sampleUserData.json'));
-    return import('../data/sampleUserData.json')
+  // return JSON.parse(await import('../data/sampleUserData.json'));
+  return import('../data/todo/sampleUserData.json');
 }
