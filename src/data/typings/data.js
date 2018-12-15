af57bf77a08804bd2f990378030bbc48e7b30a7c
bnguyensn <b.nguyen.sn@gmail.com>
2018-12-15 09:58:03 +0700

    Update data manipulation file

--- src/data/typings/data.js
+++ src/data/typings/data.js
@@ -1,11 +1,15 @@
 /**
- * This is a NodeJS function. Please call this from your console:
- * $ node src/data/typings/words-splitter.js
+ * The functions in this file are NodeJS functions. Please call them from your
+ * console:
+ * $ node src/data/typings/data.js
  * */
 
 const path = require('path');
 const fs = require('fs');
 
+/**
+ * Split a string of words using a delimiter
+ * */
 function wordsSplitter() {
   fs.readFile(path.resolve(__dirname, 'words-1.txt'), 'utf-8', (err, data) => {
     if (err) throw err;
@@ -17,4 +21,8 @@ function wordsSplitter() {
   });
 }
 
-wordsSplitter();
+function data() {
+  wordsSplitter();
+}
+
+data();
