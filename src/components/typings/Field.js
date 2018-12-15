6f9763d49bbffcb5f895a1b12932aa99e289d483
bnguyensn <b.nguyen.sn@gmail.com>
2018-12-15 17:37:47 +0700

    Add 60fps interval for <Field />

--- src/components/typings/Field.js
+++ src/components/typings/Field.js
@@ -1,6 +1,7 @@
 // @flow
 
-import * as React from 'react';
+// $FlowFixMe
+import React, { useEffect, useState } from 'react';
 import TextBlock from './TextBlock';
 import './field.css';
 
@@ -15,11 +16,39 @@ type FieldProps = {
   textBlocksData: TextBlockData[],
 };
 
+function updateField() {}
+
+function useTick() {
+  let T = null;
+
+  // $FlowFixMe
+  const [tick, setTick] = useState(1);
+
+  // $FlowFixMe
+  // Start the timer
+  useEffect(() => {
+    T = window.setTimeout(() => {
+      setTick(tick === 60 ? 1 : tick + 1);
+    }, 16.67);
+
+    return () => {
+      window.clearInterval(T);
+    };
+  });
+
+  return [tick, T];
+}
+
 function Field(props: FieldProps) {
   const { textBlocksData } = props;
 
+  const [tick, T] = useTick();
+
   return (
     <div className="field">
+      Tick: {tick}
+      <br />
+      <br />
       {textBlocksData.map(textBlockData => (
         <TextBlock
           key={textBlockData.id}
