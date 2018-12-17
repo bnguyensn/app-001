a3eaa3ebb4e55fa8e01099acb4e4dec6dc97c198
bnguyensn <b.nguyen.sn@gmail.com>
2018-12-17 11:46:14 +0700

    Update data files - Add a function to generate sample data files - Slightly adjust config file

--- src/data/typings/data.js
+++ src/data/typings/data.js
@@ -13,16 +13,59 @@ const fs = require('fs');
 function wordsSplitter() {
   fs.readFile(path.resolve(__dirname, 'words-1.txt'), 'utf-8', (err, data) => {
     if (err) throw err;
+
     const splitWords = JSON.stringify(data.split('|'));
+
     fs.writeFile(path.resolve(__dirname, './words-1.json'), splitWords, err => {
       if (err) throw err;
-      console.log('File saved successfully');
+      console.log('Words file saved successfully');
     });
   });
 }
 
+/**
+ * Generate sample JSON data file
+ * */
+function generateSampleData() {
+  fs.readFile(path.resolve(__dirname, 'words-1.json'), 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    const words = JSON.parse(data);
+
+    // Generate data for a set of random words
+    const d = [];
+    const p = [];
+    for (let i = 0; i < 10; i++) {
+      // Look for a random, unpicked array item
+      let n;
+      do {
+        n = Math.floor(Math.random() * words.length);
+      } while (p.includes(n));
+      p.push(n);
+
+      // Create the datum
+      d.push({
+        id: i,
+        text: words[n],
+        posX: 0,
+        posY: 0,
+      });
+    }
+
+    // Write data to JSON file
+    fs.writeFile(
+      path.resolve(__dirname, './sample.json'),
+      JSON.stringify(d),
+      err => {
+        if (err) throw err;
+        console.log('Sample JSON data file saved successfully');
+      },
+    );
+  });
+}
+
 function data() {
-  wordsSplitter();
+  generateSampleData();
 }
 
 data();
