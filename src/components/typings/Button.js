11539ee22b1cc21d6dc43a33116f11c9da50fb2c
bnguyensn <b.nguyen.sn@gmail.com>
2018-12-18 13:40:39 +0700

    Update tick methodology

--- src/components/typings/Button.js
+++ src/components/typings/Button.js
@@ -7,17 +7,21 @@ export default function Button(props: {
   children?: React.Node,
   action: any => any,
 }) {
-  const { children, action } = props;
+  const { children, action, ...rest } = props;
 
   return (
-    <button className="button" type="button" onClick={action}>
+    <button className="button" type="button" onClick={action} {...rest}>
       {children}
     </button>
   );
 }
 
 export function PlayButton(props: { action: any => any, playing: boolean }) {
-  const { action, playing } = props;
+  const { action, playing, ...rest } = props;
 
-  return <Button action={action}>{playing ? 'Pause' : 'Play'}</Button>;
+  return (
+    <Button action={action} {...rest}>
+      {playing ? 'Pause' : 'Play'}
+    </Button>
+  );
 }
--- src/components/typings/Game.js
+++ src/components/typings/Game.js
@@ -7,47 +7,73 @@ import { PlayButton } from './Button';
 import gameConfig from '../../data/typings/game-conf';
 import sampleData from '../../data/typings/sample';
 import './game.css';
+import TextBlock from './TextBlock';
 
 export const ConfigContext = React.createContext(gameConfig);
 
-function useTick(playing: boolean) {
-  let T = null;
-
-  const [tick, setTick] = useState(1);
-
-  useEffect(() => {
-    T = window.setTimeout(() => {
-      // Update if playing
-      if (playing) {
-        // Update frame
-        setTick(tick === gameConfig.fps ? 1 : tick + 1);
-      }
-    }, gameConfig.tickRate);
-
-    return () => {
-      window.clearInterval(T);
-    };
-  });
-
-  return [tick, T];
-}
+export type BlockType = {
+  id: number,
+  text: string,
+  posX: number,
+  posY: number,
+};
 
 export default function Game() {
+  const [tick, setTick] = useState(1);
+  const [throttling, setThrottling] = useState(false);
   const [playing, setPlaying] = useState(true);
+  const [blocks, setBlocks] = useState(sampleData);
 
-  const [tick, T] = useTick(playing);
+  const updateThrottling = () => {
+    setThrottling(true);
+    setTimeout(() => {
+      setThrottling(false);
+    }, gameConfig.tickRate);
+  };
+
+  const updateTick = () => {
+    setTick(tick === gameConfig.fps ? 1 : tick + 1);
+  };
+
+  const moveBlocksDown = () => {
+    setBlocks(blocks.map(block => ({ ...block, posY: block.posY + 1 })));
+  };
 
   const handlePlayButtonClick = () => {
     setPlaying(!playing);
-    window.clearInterval(T);
   };
+
+  //const [tick, T] = useTick(playing, moveBlocksDown);
+  useEffect(() => {
+    if (playing && !throttling) {
+      updateTick();
+      moveBlocksDown();
+      updateThrottling();
+    }
+  });
 
   return (
     <ConfigContext.Provider value={gameConfig}>
       <div className="game">
         <Field tick={tick} textBlocksData={sampleData}>
-          <PlayButton action={handlePlayButtonClick} playing={playing} />
+          <PlayButton
+            action={handlePlayButtonClick}
+            playing={playing}
+            style={{
+              position: 'absolute',
+              top: '5px',
+              right: '5px',
+            }}
+          />
           {tick}fps
+          {blocks.map(block => (
+            <TextBlock
+              key={block.id}
+              text={block.text}
+              posX={block.posX}
+              posY={block.posY}
+            />
+          ))}
         </Field>
       </div>
     </ConfigContext.Provider>
